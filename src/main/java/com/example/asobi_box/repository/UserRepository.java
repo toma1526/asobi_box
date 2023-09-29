171ed560281a78043f8c4202b0133036f65a3722
yoshinotoma <yoshinotoma@localhost>
2023-09-29 09:27:39 +0900

    ログイン機能と会員登録ページの追加

--- src/main/java/com/example/asobi_box/repository/UserRepository.java
+++ src/main/java/com/example/asobi_box/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.asobi_box.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+	public User findByEmail(String email);
 }
