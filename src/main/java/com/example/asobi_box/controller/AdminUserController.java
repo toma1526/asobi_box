05e636f6465b97c7d53358beedf351efa0d5fbe7
yoshinotoma <yoshinotoma@localhost>
2023-10-12 11:44:11 +0900

    会員詳細ページの制作

--- src/main/java/com/example/asobi_box/controller/AdminUserController.java
+++ src/main/java/com/example/asobi_box/controller/AdminUserController.java
@@ -7,6 +7,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -38,5 +39,14 @@ public class AdminUserController {
 		model.addAttribute("keyword", keyword);
 
 		return "admin/users/index";
+	}
+
+	@GetMapping("/{id}")
+	public String show(@PathVariable(name = "id") Integer id, Model model) {
+		User user = userRepository.getReferenceById(id);
+
+		model.addAttribute("user", user);
+
+		return "admin/users/show";
 	}
 }
